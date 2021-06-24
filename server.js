812571d1756797a6a4c0cfbe0ac46f2cdcfa2836
Ahmedbenabdallah29435 <benabdallah.ahmed@esprit.tn>
2021-06-24 14:32:29 +0100

    (backend/server-side)done +  Client Side Setup & Contacts UI
    i will change my work to flux structure today

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); //express we need it to The primary use of Express is to provide server-side logic for web and mobile applications
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express(); //intialize express into a var called app
 
 // Connect Database
@@ -14,10 +14,17 @@ app.get('/', (req, res) =>
 ); 
 
 // Define routes
-app.use('/api/connexion', require('./routes/connexion'))
+app.use('/api/auth', require('./routes/auth'))
 app.use('/api/inscription', require('./routes/inscription'))
 app.use('/api/gestion-des-formations', require('./routes/gestion-des-formations'))
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const PORT =5000;//look for an envirement var first or any port we want process.env.PORT ||
 
 app.listen(PORT, () => console.log(`Server staarted on port ${PORT}`)); //listen method and we need a port to listen on
